7a0e6578915dbb0f2e12ce3b533cf999a7bea819
Reomor <reomor@gmail.com>
2021-03-01 21:06:49 +0300

    hw00 jacoco reports

---
+++ buildSrc/src/main/kotlin/myproject.jacoco-aggregation.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    java
+    jacoco
+}
+
+repositories {
+    jcenter()
+}
+
+// A resolvable configuration to collect source code
+val sourcesPath: Configuration by configurations.creating {
+    isVisible = false
+    isCanBeResolved = true
+    isCanBeConsumed = false
+    extendsFrom(configurations.implementation.get())
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
+        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named("source-folders"))
+    }
+}
+
+// A resolvable configuration to collect JaCoCo coverage data
+val coverageDataPath: Configuration by configurations.creating {
+    isVisible = false
+    isCanBeResolved = true
+    isCanBeConsumed = false
+    extendsFrom(configurations.implementation.get())
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
+        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named("jacoco-coverage-data"))
+    }
+}
+
+// Task to gather code coverage from multiple subprojects
+val codeCoverageReport by tasks.registering(JacocoReport::class) {
+
+    val matching = configurations.runtimeClasspath.get()
+        .asFileTree
+        .filter { file -> file.isFile && file.path.contains(projectDir.parent) }
+    matching.map { file -> println(file) }
+    additionalClassDirs(matching)
+    additionalSourceDirs(sourcesPath.incoming.artifactView { lenient(true) }.files)
+    executionData(coverageDataPath.incoming.artifactView { lenient(true) }.files.filter { it.exists() })
+
+    reports {
+        // xml is usually used to integrate code coverage with
+        // other tools like SonarQube, Coveralls or Codecov
+        xml.isEnabled = true
+
+        // HTML reports can be used to see code coverage
+        // without any external tools
+        html.isEnabled = true
+    }
+//    onlyIf {
+//        true
+//    }
+
+//    afterEvaluate {
+//        classDirectories.setFrom(
+//            sourceSets.main.get().output.asFileTree.matching {
+//                exclude("kotlin/reflect/*")
+//            }
+//        )
+//    }
+}
+
+// Make JaCoCo report generation part of the 'check' lifecycle phase
+tasks.check {
+    dependsOn(codeCoverageReport)
+}
