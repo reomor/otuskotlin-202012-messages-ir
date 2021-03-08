6a87186c08896468e143f293f6f079bb1de5479e
Reomor <reomor@gmail.com>
2021-03-08 23:49:35 +0300

    hw03 create channel response serialization tests

--- ok-multiplatform-mapping/src/main/kotlin/ru/otus/kotlin/messaging/mapper/openapi/serialization.kt
+++ ok-multiplatform-mapping/src/main/kotlin/ru/otus/kotlin/messaging/mapper/openapi/serialization.kt
@@ -1,6 +1,7 @@
 package ru.otus.kotlin.messaging.mapper.openapi
 
 import kotlinx.serialization.*
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
@@ -10,20 +11,22 @@ import kotlinx.serialization.modules.contextual
 import kotlinx.serialization.modules.plus
 import kotlinx.serialization.modules.polymorphic
 import ru.otus.kotlin.messaging.api.model.message.serialization.requestResponseSerializersModules
-import ru.otus.kotlin.messaging.openapi.channel.models.BaseMessage
-import ru.otus.kotlin.messaging.openapi.channel.models.ChannelDto
-import ru.otus.kotlin.messaging.openapi.channel.models.ChannelType
-import ru.otus.kotlin.messaging.openapi.channel.models.CreateChannelRequest
+import ru.otus.kotlin.messaging.openapi.channel.models.*
 import java.util.*
 
 @ExperimentalSerializationApi
 private val openApiRequestResponseSerializersModules = SerializersModule {
     polymorphic(BaseMessage::class) {
         subclass(CreateChannelRequest::class, CreateChannelRequestSerializer)
+        subclass(CreateChannelResponse::class, CreateChannelResponseSerializer)
     }
     contextual(ChannelTypeSerializer)
     contextual(ChannelDtoSerializer)
+    contextual(ResponseStatusSerializer)
+    contextual(ErrorSeveritySerializer)
+    contextual(ErrorDtoSerializer)
     contextual(CreateChannelRequestSerializer)
+    contextual(CreateChannelResponseSerializer)
     contextual(UUIDSerializer)
 }
 
@@ -36,23 +39,6 @@ val generalRequestResponseSerializer: Json by lazy {
         classDiscriminator = "type"
     }
 }
-
-//object BaseMessageSerializer : KSerializer<BaseMessage> {
-//
-//    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("BaseMessage") {
-//    }
-//
-//    override fun serialize(encoder: Encoder, value: BaseMessage) {
-//        when (value.type) {
-//            "CreateChannelRequest" -> Json.encodeToString(value as CreateChannelRequest)
-//        }
-//    }
-//
-//    override fun deserialize(decoder: Decoder): BaseMessage {
-//        val string = decoder.decodeString()
-//        return Json.decodeFromString(string)
-//    }
-//}
 
 @ExperimentalSerializationApi
 @Serializer(forClass = CreateChannelRequest::class)
@@ -80,9 +66,9 @@ object CreateChannelRequestSerializer : KSerializer<CreateChannelRequest> {
             var channel: ChannelDto? = null
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
-                    0 -> requestId = decodeStringElement(descriptor, 1)
-                    1 -> requestTime = decodeStringElement(descriptor, 2)
-                    2 -> channel = decodeSerializableElement(descriptor, 2, ChannelDtoSerializer)
+                    0 -> requestId = decodeStringElement(descriptor, index)
+                    1 -> requestTime = decodeStringElement(descriptor, index)
+                    2 -> channel = decodeSerializableElement(descriptor, index, ChannelDtoSerializer)
                     CompositeDecoder.DECODE_DONE -> break
                     else -> error("Unexpected index: $index")
                 }
@@ -91,6 +77,63 @@ object CreateChannelRequestSerializer : KSerializer<CreateChannelRequest> {
                 type = "CreateChannelRequest",
                 requestId = requestId,
                 requestTime = requestTime,
+                channel = channel
+            )
+        }
+}
+
+@ExperimentalSerializationApi
+@Serializer(forClass = CreateChannelResponse::class)
+object CreateChannelResponseSerializer : KSerializer<CreateChannelResponse> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("CreateChannelResponse") {
+            element<String>(elementName = "responseId")
+            element<String>(elementName = "responseTime")
+            element(elementName = "errors", ListSerializer(ErrorDtoSerializer).descriptor)
+            element(elementName = "status", ResponseStatusSerializer.descriptor)
+            element(elementName = "request", CreateChannelRequestSerializer.descriptor)
+            element(elementName = "channel", ChannelDtoSerializer.descriptor)
+        }
+
+    override fun serialize(encoder: Encoder, value: CreateChannelResponse) {
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value.responseId ?: "")
+            encodeStringElement(descriptor, 1, value.responseTime ?: "")
+            encodeNullableSerializableElement(descriptor, 2, ListSerializer(ErrorDtoSerializer), value.errors)
+            encodeNullableSerializableElement(descriptor, 3, ResponseStatusSerializer, value.status)
+            encodeNullableSerializableElement(descriptor, 4, CreateChannelRequestSerializer, value.request as CreateChannelRequest)
+            encodeNullableSerializableElement(descriptor, 5, ChannelDtoSerializer, value.channel)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): CreateChannelResponse =
+        decoder.decodeStructure(descriptor) {
+            var responseId: String? = null
+            var responseTime: String? = null
+            var errors: List<ErrorDto> = emptyList()
+            var status: ResponseStatus? = null
+            var request: BaseMessage? = null
+            var channel: ChannelDto? = null
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> responseId = decodeStringElement(descriptor, index)
+                    1 -> responseTime = decodeStringElement(descriptor, index)
+                    2 -> errors = decodeSerializableElement(descriptor, index, ListSerializer(ErrorDtoSerializer))
+                    3 -> status = decodeSerializableElement(descriptor, index, ResponseStatusSerializer)
+                    4 -> request = decodeSerializableElement(descriptor, index, CreateChannelRequestSerializer)
+                    5 -> channel = decodeSerializableElement(descriptor, index, ChannelDtoSerializer)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+            CreateChannelResponse(
+                type = "CreateChannelResponse",
+                responseId = responseId,
+                responseTime = responseTime,
+                errors = errors,
+                status = status,
+                request = request,
                 channel = channel
             )
         }
@@ -171,3 +214,69 @@ object ChannelTypeSerializer : KSerializer<ChannelType> {
     override fun deserialize(decoder: Decoder): ChannelType =
         ChannelType.valueOf(serializer.deserialize(decoder))
 }
+
+object ResponseStatusSerializer : KSerializer<ResponseStatus> {
+    private val serializer = String.serializer()
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("ResponseStatus", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: ResponseStatus) =
+        serializer.serialize(encoder, value.name)
+
+    override fun deserialize(decoder: Decoder): ResponseStatus =
+        ResponseStatus.valueOf(serializer.deserialize(decoder))
+}
+
+object ErrorSeveritySerializer : KSerializer<ErrorSeverity> {
+    private val serializer = String.serializer()
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("ErrorSeverity", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: ErrorSeverity) =
+        serializer.serialize(encoder, value.name)
+
+    override fun deserialize(decoder: Decoder): ErrorSeverity =
+        ErrorSeverity.valueOf(serializer.deserialize(decoder))
+}
+
+object ErrorDtoSerializer : KSerializer<ErrorDto> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("ErrorDto") {
+            element<String>(elementName = "code")
+            element<ErrorSeverity>(elementName = "level")
+            element<String>(elementName = "message")
+        }
+
+    @ExperimentalSerializationApi
+    override fun serialize(encoder: Encoder, value: ErrorDto) =
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value.code ?: "")
+            encodeNullableSerializableElement(descriptor, 1, ErrorSeveritySerializer, value.level)
+            encodeStringElement(descriptor, 2, value.message ?: "")
+        }
+
+    override fun deserialize(decoder: Decoder): ErrorDto =
+        decoder.decodeStructure(descriptor) {
+            var code: String? = null
+            var level: ErrorSeverity? = null
+            var message: String? = null
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> code = decodeStringElement(descriptor, 0)
+                    1 -> level = decodeSerializableElement(descriptor, 1, ErrorSeveritySerializer)
+                    2 -> message = decodeStringElement(descriptor, 2)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+            ErrorDto(
+                code = code,
+                level = level,
+                message = message
+            )
+        }
+}
+
--- ok-multiplatform-mapping/src/test/kotlin/ru/otus/kotlin/messaging/mapper/OpenApiSerialization.kt
+++ ok-multiplatform-mapping/src/test/kotlin/ru/otus/kotlin/messaging/mapper/OpenApiSerialization.kt
@@ -5,10 +5,7 @@ import kotlinx.serialization.encodeToString
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import ru.otus.kotlin.messaging.mapper.openapi.generalRequestResponseSerializer
-import ru.otus.kotlin.messaging.openapi.channel.models.BaseMessage
-import ru.otus.kotlin.messaging.openapi.channel.models.ChannelDto
-import ru.otus.kotlin.messaging.openapi.channel.models.ChannelType
-import ru.otus.kotlin.messaging.openapi.channel.models.CreateChannelRequest
+import ru.otus.kotlin.messaging.openapi.channel.models.*
 import java.time.LocalDateTime
 import java.util.*
 import kotlin.test.assertEquals
@@ -33,6 +30,7 @@ internal class OpenApiSerialization {
 
     @Test
     fun testChannelDtoSerialization() {
+
         val expected = ChannelDto(
             id = UUID.randomUUID().toString(),
             name = "channelName",
@@ -48,12 +46,14 @@ internal class OpenApiSerialization {
 
     @Test
     fun testCreateChannelRequestSerialization() {
+
         val channel = ChannelDto(
             id = UUID.randomUUID().toString(),
             name = "channelName",
             ownerId = UUID.randomUUID().toString(),
             type = ChannelType.PUBLIC_CHANNEL
         )
+
         val expected = CreateChannelRequest(
             type = "CreateChannelRequest",
             requestId = UUID.randomUUID().toString(),
@@ -67,9 +67,9 @@ internal class OpenApiSerialization {
         assertEquals(expected, actual)
     }
 
-//    @Disabled
     @Test
     fun testCreateChannelRequestPolymorphSerialization() {
+
         val channel = ChannelDto(
             id = UUID.randomUUID().toString(),
             name = "channelName",
@@ -85,6 +85,101 @@ internal class OpenApiSerialization {
 
         val encodeToString = generalRequestResponseSerializer.encodeToString(expected)
         val actual = generalRequestResponseSerializer.decodeFromString<CreateChannelRequest>(encodeToString)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testResponseStatusSerialization() {
+        val expected = ResponseStatus.INTERNAL_SERVER_ERROR
+        val encodeToString = generalRequestResponseSerializer.encodeToString(expected)
+        val actual = generalRequestResponseSerializer.decodeFromString<ResponseStatus>(encodeToString)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testErrorSeveritySerialization() {
+        val expected = ErrorSeverity.INFO
+        val encodeToString = generalRequestResponseSerializer.encodeToString(expected)
+        val actual = generalRequestResponseSerializer.decodeFromString<ErrorSeverity>(encodeToString)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testErrorDtoSerialization() {
+
+        val expected = ErrorDto(
+            code = "666",
+            level = ErrorSeverity.INFO,
+            message = "message"
+        )
+
+        val encodeToString = generalRequestResponseSerializer.encodeToString(expected)
+        val actual = generalRequestResponseSerializer.decodeFromString<ErrorDto>(encodeToString)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testListErrorDtoSerialization() {
+
+        val expected = listOf(
+            ErrorDto(
+                code = "666",
+                level = ErrorSeverity.INFO,
+                message = "info message"
+            ),
+            ErrorDto(
+                code = "777",
+                level = ErrorSeverity.FATAL,
+                message = "fatal message"
+            ),
+        )
+
+        val encodeToString = generalRequestResponseSerializer.encodeToString(expected)
+        val actual = generalRequestResponseSerializer.decodeFromString<List<ErrorDto>>(encodeToString)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testCreateChannelResponseSerialization() {
+
+        val channel = ChannelDto(
+            id = UUID.randomUUID().toString(),
+            name = "channelName",
+            ownerId = UUID.randomUUID().toString(),
+            type = ChannelType.PUBLIC_CHANNEL
+        )
+
+        val request = CreateChannelRequest(
+            type = "CreateChannelRequest",
+            requestId = UUID.randomUUID().toString(),
+            requestTime = LocalDateTime.now().toString(),
+            channel = channel
+        )
+
+        val expected = CreateChannelResponse(
+            type = "CreateChannelResponse",
+            responseId = UUID.randomUUID().toString(),
+            responseTime = LocalDateTime.now().toString(),
+            errors = listOf(
+                ErrorDto(
+                    code = "666",
+                    level = ErrorSeverity.INFO,
+                    message = "info message"
+                ),
+                ErrorDto(
+                    code = "777",
+                    level = ErrorSeverity.FATAL,
+                    message = "fatal message"
+                ),
+            ),
+            status = ResponseStatus.BAD_REQUEST,
+            request = request,
+            channel = channel
+        )
+
+        val encodeToString = generalRequestResponseSerializer.encodeToString(expected)
+        val actual = generalRequestResponseSerializer.decodeFromString<CreateChannelResponse>(encodeToString)
 
         assertEquals(expected, actual)
     }
