49fe8bf590da1c857a38099be50506404d5ddc19
Reomor <reomor@gmail.com>
2021-03-21 15:23:52 +0300

    hw03 custom serializers for openapi models

--- ok-multiplatform-mapping/src/main/kotlin/ru/otus/kotlin/messaging/mapper/openapi/serialization.kt
+++ ok-multiplatform-mapping/src/main/kotlin/ru/otus/kotlin/messaging/mapper/openapi/serialization.kt
@@ -19,15 +19,23 @@ private val openApiRequestResponseSerializersModules = SerializersModule {
     polymorphic(BaseMessage::class) {
         subclass(CreateChannelRequest::class, CreateChannelRequestSerializer)
         subclass(CreateChannelResponse::class, CreateChannelResponseSerializer)
+        subclass(DeleteChannelRequest::class, DeleteChannelRequestSerializer)
+        subclass(DeleteChannelResponse::class, DeleteChannelResponseSerializer)
+        subclass(GetChannelRequest::class, GetChannelRequestSerializer)
+        subclass(GetChannelResponse::class, GetChannelResponseSerializer)
     }
     contextual(ChannelTypeSerializer)
     contextual(ChannelDtoSerializer)
+    contextual(ChannelFilterDtoSerializer)
     contextual(ResponseStatusSerializer)
     contextual(ErrorSeveritySerializer)
     contextual(ErrorDtoSerializer)
     contextual(CreateChannelRequestSerializer)
     contextual(CreateChannelResponseSerializer)
-    contextual(UUIDSerializer)
+    contextual(DeleteChannelRequestSerializer)
+    contextual(DeleteChannelResponseSerializer)
+    contextual(GetChannelRequestSerializer)
+    contextual(GetChannelResponseSerializer)
 }
 
 val generalRequestResponseSerializer: Json by lazy {
@@ -62,9 +70,11 @@ object CreateChannelRequestSerializer : KSerializer<CreateChannelRequest> {
 
     override fun deserialize(decoder: Decoder): CreateChannelRequest =
         decoder.decodeStructure(descriptor) {
+
             var requestId: String? = null
             var requestTime: String? = null
             var channel: ChannelDto? = null
+
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
                     0 -> requestId = decodeStringElement(descriptor, index)
@@ -74,6 +84,7 @@ object CreateChannelRequestSerializer : KSerializer<CreateChannelRequest> {
                     else -> error("Unexpected index: $index")
                 }
             }
+
             CreateChannelRequest(
                 type = "CreateChannelRequest",
                 requestId = requestId,
@@ -103,19 +114,26 @@ object CreateChannelResponseSerializer : KSerializer<CreateChannelResponse> {
             encodeStringElement(descriptor, 1, value.responseTime ?: "")
             encodeNullableSerializableElement(descriptor, 2, ListSerializer(ErrorDtoSerializer), value.errors)
             encodeNullableSerializableElement(descriptor, 3, ResponseStatusSerializer, value.status)
-            encodeNullableSerializableElement(descriptor, 4, CreateChannelRequestSerializer, value.request as CreateChannelRequest)
+            encodeNullableSerializableElement(
+                descriptor,
+                4,
+                CreateChannelRequestSerializer,
+                value.request as CreateChannelRequest
+            )
             encodeNullableSerializableElement(descriptor, 5, ChannelDtoSerializer, value.channel)
         }
     }
 
     override fun deserialize(decoder: Decoder): CreateChannelResponse =
         decoder.decodeStructure(descriptor) {
+
             var responseId: String? = null
             var responseTime: String? = null
             var errors: List<ErrorDto> = emptyList()
             var status: ResponseStatus? = null
             var request: BaseMessage? = null
             var channel: ChannelDto? = null
+
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
                     0 -> responseId = decodeStringElement(descriptor, index)
@@ -128,6 +146,7 @@ object CreateChannelResponseSerializer : KSerializer<CreateChannelResponse> {
                     else -> error("Unexpected index: $index")
                 }
             }
+
             CreateChannelResponse(
                 type = "CreateChannelResponse",
                 responseId = responseId,
@@ -136,6 +155,225 @@ object CreateChannelResponseSerializer : KSerializer<CreateChannelResponse> {
                 status = status,
                 request = request,
                 channel = channel
+            )
+        }
+}
+
+@ExperimentalSerializationApi
+@Serializer(forClass = DeleteChannelRequest::class)
+object DeleteChannelRequestSerializer : KSerializer<DeleteChannelRequest> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("DeleteChannelRequest") {
+            element<String>(elementName = "requestId")
+            element<String>(elementName = "requestTime")
+            element<String>(elementName = "channelId")
+        }
+
+    override fun serialize(encoder: Encoder, value: DeleteChannelRequest) {
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value.requestId ?: "")
+            encodeStringElement(descriptor, 1, value.requestTime ?: "")
+            encodeStringElement(descriptor, 2, value.channelId ?: "")
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): DeleteChannelRequest =
+        decoder.decodeStructure(descriptor) {
+
+            var requestId: String? = null
+            var requestTime: String? = null
+            var channelId: String? = null
+
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> requestId = decodeStringElement(descriptor, index)
+                    1 -> requestTime = decodeStringElement(descriptor, index)
+                    2 -> channelId = decodeStringElement(descriptor, index)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+            DeleteChannelRequest(
+                type = "DeleteChannelRequest",
+                requestId = requestId,
+                requestTime = requestTime,
+                channelId = channelId
+            )
+        }
+}
+
+@ExperimentalSerializationApi
+@Serializer(forClass = DeleteChannelResponse::class)
+object DeleteChannelResponseSerializer : KSerializer<DeleteChannelResponse> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("DeleteChannelResponse") {
+            element<String>(elementName = "responseId")
+            element<String>(elementName = "responseTime")
+            element(elementName = "errors", ListSerializer(ErrorDtoSerializer).descriptor)
+            element(elementName = "status", ResponseStatusSerializer.descriptor)
+            element(elementName = "request", CreateChannelRequestSerializer.descriptor)
+            element(elementName = "channel", ChannelDtoSerializer.descriptor)
+        }
+
+    override fun serialize(encoder: Encoder, value: DeleteChannelResponse) {
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value.responseId ?: "")
+            encodeStringElement(descriptor, 1, value.responseTime ?: "")
+            encodeNullableSerializableElement(descriptor, 2, ListSerializer(ErrorDtoSerializer), value.errors)
+            encodeNullableSerializableElement(descriptor, 3, ResponseStatusSerializer, value.status)
+            encodeNullableSerializableElement(
+                descriptor,
+                4,
+                DeleteChannelRequestSerializer,
+                value.request as DeleteChannelRequest
+            )
+            encodeNullableSerializableElement(descriptor, 5, ChannelDtoSerializer, value.channel)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): DeleteChannelResponse =
+        decoder.decodeStructure(descriptor) {
+
+            var responseId: String? = null
+            var responseTime: String? = null
+            var errors: List<ErrorDto> = emptyList()
+            var status: ResponseStatus? = null
+            var request: BaseMessage? = null
+            var channel: ChannelDto? = null
+
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> responseId = decodeStringElement(descriptor, index)
+                    1 -> responseTime = decodeStringElement(descriptor, index)
+                    2 -> errors = decodeSerializableElement(descriptor, index, ListSerializer(ErrorDtoSerializer))
+                    3 -> status = decodeSerializableElement(descriptor, index, ResponseStatusSerializer)
+                    4 -> request = decodeSerializableElement(descriptor, index, DeleteChannelRequestSerializer)
+                    5 -> channel = decodeSerializableElement(descriptor, index, ChannelDtoSerializer)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+
+            DeleteChannelResponse(
+                type = "DeleteChannelResponse",
+                responseId = responseId,
+                responseTime = responseTime,
+                errors = errors,
+                status = status,
+                request = request,
+                channel = channel
+            )
+        }
+}
+
+@ExperimentalSerializationApi
+@Serializer(forClass = GetChannelRequest::class)
+object GetChannelRequestSerializer : KSerializer<GetChannelRequest> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("GetChannelRequest") {
+            element<String>(elementName = "requestId")
+            element<String>(elementName = "requestTime")
+            element(elementName = "filter", ChannelFilterDtoSerializer.descriptor)
+        }
+
+    override fun serialize(encoder: Encoder, value: GetChannelRequest) {
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value.requestId ?: "")
+            encodeStringElement(descriptor, 1, value.requestTime ?: "")
+            encodeNullableSerializableElement(descriptor, 2, ChannelFilterDtoSerializer, value.filter)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): GetChannelRequest =
+        decoder.decodeStructure(descriptor) {
+
+            var requestId: String? = null
+            var requestTime: String? = null
+            var filter: ChannelFilterDto? = null
+
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> requestId = decodeStringElement(descriptor, index)
+                    1 -> requestTime = decodeStringElement(descriptor, index)
+                    2 -> filter = decodeSerializableElement(descriptor, index, ChannelFilterDtoSerializer)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+
+            GetChannelRequest(
+                type = "GetChannelRequest",
+                requestId = requestId,
+                requestTime = requestTime,
+                filter = filter
+            )
+        }
+}
+
+@ExperimentalSerializationApi
+@Serializer(forClass = GetChannelResponse::class)
+object GetChannelResponseSerializer : KSerializer<GetChannelResponse> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("GetChannelResponse") {
+            element<String>(elementName = "responseId")
+            element<String>(elementName = "responseTime")
+            element(elementName = "errors", ListSerializer(ErrorDtoSerializer).descriptor)
+            element(elementName = "status", ResponseStatusSerializer.descriptor)
+            element(elementName = "request", CreateChannelRequestSerializer.descriptor)
+            element(elementName = "channel", ListSerializer(ChannelDtoSerializer).descriptor)
+        }
+
+    override fun serialize(encoder: Encoder, value: GetChannelResponse) {
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value.responseId ?: "")
+            encodeStringElement(descriptor, 1, value.responseTime ?: "")
+            encodeNullableSerializableElement(descriptor, 2, ListSerializer(ErrorDtoSerializer), value.errors)
+            encodeNullableSerializableElement(descriptor, 3, ResponseStatusSerializer, value.status)
+            encodeNullableSerializableElement(
+                descriptor,
+                4,
+                GetChannelRequestSerializer,
+                value.request as GetChannelRequest
+            )
+            encodeNullableSerializableElement(descriptor, 5, ListSerializer(ChannelDtoSerializer), value.channels)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): GetChannelResponse =
+        decoder.decodeStructure(descriptor) {
+
+            var responseId: String? = null
+            var responseTime: String? = null
+            var errors: List<ErrorDto> = emptyList()
+            var status: ResponseStatus? = null
+            var request: BaseMessage? = null
+            var channels: List<ChannelDto> = emptyList()
+
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> responseId = decodeStringElement(descriptor, index)
+                    1 -> responseTime = decodeStringElement(descriptor, index)
+                    2 -> errors = decodeSerializableElement(descriptor, index, ListSerializer(ErrorDtoSerializer))
+                    3 -> status = decodeSerializableElement(descriptor, index, ResponseStatusSerializer)
+                    4 -> request = decodeSerializableElement(descriptor, index, GetChannelRequestSerializer)
+                    5 -> channels = decodeSerializableElement(descriptor, index, ListSerializer(ChannelDtoSerializer))
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+
+            GetChannelResponse(
+                type = "GetChannelResponse",
+                responseId = responseId,
+                responseTime = responseTime,
+                errors = errors,
+                status = status,
+                request = request,
+                channels = channels
             )
         }
 }
@@ -166,20 +404,23 @@ object ChannelDtoSerializer : KSerializer<ChannelDto> {
 
     override fun deserialize(decoder: Decoder): ChannelDto =
         decoder.decodeStructure(descriptor) {
+
             var id: String? = null
             var name: String? = null
             var ownerId: String? = null
             var channelType: ChannelType? = null
+
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
-                    0 -> id = decodeStringElement(descriptor, 0)
-                    1 -> name = decodeStringElement(descriptor, 1)
-                    2 -> ownerId = decodeStringElement(descriptor, 2)
-                    3 -> channelType = decodeSerializableElement(descriptor, 2, ChannelTypeSerializer)
+                    0 -> id = decodeStringElement(descriptor, index)
+                    1 -> name = decodeStringElement(descriptor, index)
+                    2 -> ownerId = decodeStringElement(descriptor, index)
+                    3 -> channelType = decodeSerializableElement(descriptor, index, ChannelTypeSerializer)
                     CompositeDecoder.DECODE_DONE -> break
                     else -> error("Unexpected index: $index")
                 }
             }
+
             ChannelDto(
                 id = id,
                 name = name,
@@ -189,20 +430,52 @@ object ChannelDtoSerializer : KSerializer<ChannelDto> {
         }
 }
 
-object UUIDSerializer : KSerializer<UUID> {
-    private val serializer = String.serializer()
+object ChannelFilterDtoSerializer : KSerializer<ChannelFilterDto> {
 
-    override val descriptor = serializer.descriptor
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("ChannelFilterDto") {
+            element(elementName = "channelIds", ListSerializer(String.serializer()).descriptor)
+            element<Int>(elementName = "pageSize")
+            element<Int>(elementName = "pageNumber")
+        }
 
-    override fun serialize(encoder: Encoder, value: UUID) =
-        serializer.serialize(encoder, value.toString())
+    override fun serialize(encoder: Encoder, value: ChannelFilterDto) =
+        encoder.encodeStructure(descriptor) {
+            encodeNullableSerializableElement(
+                descriptor,
+                0,
+                ListSerializer(String.serializer()),
+                value.channelIds
+            )
+            encodeIntElement(descriptor, 1, value.pageSize ?: 0)
+            encodeIntElement(descriptor, 2, value.pageNumber ?: 0)
+        }
 
-    override fun deserialize(decoder: Decoder) =
-        UUID.fromString(serializer.deserialize(decoder))
+    override fun deserialize(decoder: Decoder): ChannelFilterDto =
+        decoder.decodeStructure(descriptor) {
+
+            var channelIds: List<String> = emptyList()
+            var pageSize: Int? = null
+            var pageNumber: Int? = null
+
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> channelIds = decodeSerializableElement(descriptor, index, ListSerializer(String.serializer()))
+                    1 -> pageSize = decodeIntElement(descriptor, index)
+                    2 -> pageNumber = decodeIntElement(descriptor, index)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+
+            ChannelFilterDto(
+                channelIds = channelIds,
+                pageSize = pageSize,
+                pageNumber = pageNumber
+            )
+        }
 }
 
-//@ExperimentalSerializationApi
-//@Serializer(forClass = ChannelType::class)
 object ChannelTypeSerializer : KSerializer<ChannelType> {
     private val serializer = String.serializer()
 
@@ -266,9 +539,9 @@ object ErrorDtoSerializer : KSerializer<ErrorDto> {
             var message: String? = null
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
-                    0 -> code = decodeStringElement(descriptor, 0)
-                    1 -> level = decodeSerializableElement(descriptor, 1, ErrorSeveritySerializer)
-                    2 -> message = decodeStringElement(descriptor, 2)
+                    0 -> code = decodeStringElement(descriptor, index)
+                    1 -> level = decodeSerializableElement(descriptor, index, ErrorSeveritySerializer)
+                    2 -> message = decodeStringElement(descriptor, index)
                     CompositeDecoder.DECODE_DONE -> break
                     else -> error("Unexpected index: $index")
                 }
@@ -280,4 +553,3 @@ object ErrorDtoSerializer : KSerializer<ErrorDto> {
             )
         }
 }
-
